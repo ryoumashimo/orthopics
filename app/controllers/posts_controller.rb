c684fdc8e7eb22fa44183071e2f5aefab5c8fa2f
ryoumashimo <ryoumashimo0711@gmail.comm>
2020-08-01 19:01:44 +0900

    create mypage

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -26,13 +26,14 @@ class PostsController < ApplicationController
   end
 
   def update
-    post = Post.fin(params[:id])
+    post = Post.find(params[:id])
     post.update(post_params)
+    redirect_to root_path
   end
 
   private
   def post_params
-    params.require(:post).permit(:image, :text)
+    params.require(:post).permit(:image, :text).merge(user_id: current_user.id)
   end
 
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_07_25_021448) do
+ActiveRecord::Schema.define(version: 2020_08_01_083603) do
 
   create_table "posts", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "text"
     t.string "image", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "user_id"
   end
 
   create_table "users", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
